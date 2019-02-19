282d7d116684fee8c3e3821547c8d7ad8b756908
YunusDonmez <y.donmez.3237@gmail.com>
2019-02-19 22:49:24 +0300

    allow-unrelated-histories

--- app/src/main/java/com/genesis/randomphoto/MainActivity.kt
+++ app/src/main/java/com/genesis/randomphoto/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.genesis.randomphoto
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
